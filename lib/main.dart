c745e5a3f28f657f21e750cbe22eb3df966ed572
Hazhir2002 <HazhirYousefi2002@gmail.com>
2025-02-20 17:41:49 +0330

    splash screen finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Spotify',
       theme: AppTheme.lightTheme,
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
     );
   }
 }
